a50640a1845139468932ab2b7d108fb4134ecad8
tofaelkabir <tofaelkabir@gmail.com>
2020-02-25 21:32:06 -0500

    0225202-2

---
+++ src/collectionFramework/UseOfArrayList01b.java
@@ -0,0 +1,35 @@
+package collectionFramework;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class UseOfArrayList01b {
+	/*
+	 * Author: Shohag, Organised by: Tofael
+	 */
+	public static void main(String[] args) {
+
+		// getting new ArrayList -by default constructor-->initial capacity (ic) 10
+		ArrayList ar = new ArrayList();
+		ar.add("Shohag"); // index [0]
+		ar.add("Tofael");
+		ar.add(5); // different data type is allowed, here int is used
+		ar.add(""); // empty is allowed for String Type
+		ar.add("Manir");
+		ar.add("Orfat");
+		ar.add("Tofael");// duplicate value is allowed
+		ar.add("Imran");
+		ar.add(5.5); // ? type
+		ar.add('M'); // different data type is allowed, here char is used
+		ar.add("Nafasat");
+		ar.add("Mohammad");// ic is [10]-- then capacity will be increased to [ic*(3/2)+1]=16
+
+		System.out.println("\n^^^^^^^^^^^^^ After using Iterator the new Array List ^^^^^^^^^^^^^^^^ ");
+		Iterator iterator = ar.iterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+
+	}
+}
